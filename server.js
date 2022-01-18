4d0e6149f36d460b2355938f76cbff56e59aa552
Andres Sanchez <asdelahoz@hotmail.com>
2022-01-18 02:37:53 -0500

    initial commit

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/atictoctae'));
+app.use(express.static('./dist/atictoctae/src'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/atictoctae/'}),
+    res.sendFile('index.html', {root: 'dist/atictoctae/src/'}),
 );
 
 // Start the app by listening on the default Heroku port
